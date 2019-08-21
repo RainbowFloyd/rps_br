34d223450c1be5b2797a52de519d0eb3683cf6a2
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-08-21 13:35:00 -0700

    Changes App to class component
    This is in preperation to use state for username and other stats

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <p>Welcome to Rock, Paper, Scissors Battle Royal!</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to Rock, Paper, Scissors Battle Royal!</p>
+      </div>
+    )
+  }
 }
 
 export default App;

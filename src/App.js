7f5e933dfa7dabd0a73a890b4a6ff9ecd5b94949
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-08-22 02:15:59 -0700

    Adds main menu button to route to main menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import MainMenu from './Components/MainMenu/MainMenu';
 import ChooseGame from './Components/ChooseGame/ChooseGame';
@@ -11,6 +11,7 @@ class App extends Component {
     return (
       <div>
         <p>Welcome to Rock, Paper, Scissors Battle Royal!</p>
+        <button><Link to='/'>Main Menu</Link></button>
         <Route exact path='/' component={MainMenu} />
         <Route path='/gamemode' component={ChooseGame} />
         <Route path='/options' component={Options} />

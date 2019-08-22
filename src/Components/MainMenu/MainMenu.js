41044c6a54064e205455c3afcf4f51b40299e050
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-08-22 02:04:54 -0700

    Adds routes to main menu buttons

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+
+import ChooseGame from '../ChooseGame/ChooseGame';
+import Options from '../Options/Options';
 
 const MainMenu = () => {
 	return (
 		<div>
 			<button><Link to='/gamemode'>Choose Gamemode</Link></button>
 			<button><Link to='/options'>Options</Link></button>
+			<Route path='/gamemode' component={ChooseGame} />
+			<Route path='/options' component={Options} />
 		</div>
 	)
 }

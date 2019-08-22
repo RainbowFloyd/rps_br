79105bb142fb8f05f3d1c564e6f9eda5e81893f4
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-08-22 02:12:58 -0700

    Moves routes from main menu to app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 import MainMenu from './Components/MainMenu/MainMenu';
+import ChooseGame from './Components/ChooseGame/ChooseGame';
+import Options from './Components/Options/Options';
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>Welcome to Rock, Paper, Scissors Battle Royal!</p>
-        <MainMenu />
+        <Route exact path='/' component={MainMenu} />
+        <Route path='/gamemode' component={ChooseGame} />
+        <Route path='/options' component={Options} />
       </div>
     )
   }
--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
-
-import ChooseGame from '../ChooseGame/ChooseGame';
-import Options from '../Options/Options';
+import { Link } from 'react-router-dom';
 
 const MainMenu = () => {
 	return (
 		<div>
 			<button><Link to='/gamemode'>Choose Gamemode</Link></button>
 			<button><Link to='/options'>Options</Link></button>
-			<Route path='/gamemode' component={ChooseGame} />
-			<Route path='/options' component={Options} />
 		</div>
 	)
 }

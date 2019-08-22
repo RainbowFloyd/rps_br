a3e7a3cb1e3b2051f63c996556743ee050aba582
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-08-22 01:32:36 -0700

    Fixed typo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Main_menu from './Components/MainMenu/MainMenu';
+import MainMenu from './Components/MainMenu/MainMenu';
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>Welcome to Rock, Paper, Scissors Battle Royal!</p>
-        <Main_menu />
+        <MainMenu />
       </div>
     )
   }

0ca0fffe121085680bab90b652b5a7da702f8229
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-08-21 18:48:08 -0700

    Adds main menu component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Main_menu from './Components/MainMenu/MainMenu';
+
 class App extends Component {
   render() {
     return (
       <div>
         <p>Welcome to Rock, Paper, Scissors Battle Royal!</p>
+        <Main_menu />
       </div>
     )
   }
---
+++ src/Components/MainMenu/MainMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainMenu = () => {
+	return (
+		<div>
+			<button>Choose Gamemode</button>
+			<button>Options</button>
+		</div>
+	)
+}
+
+export default MainMenu;

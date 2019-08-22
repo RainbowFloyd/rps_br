163377f284451672bbc28ddda036a52c8537a67d
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-08-22 12:56:50 -0700

    Adds buttons to make rps choice

--- src/Components/Battle/Battle.js
+++ src/Components/Battle/Battle.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+const rpsArr = ['Rock', 'Paper', 'Scissors'];
+
+const handleChoice = (e) => {
+	console.log(e.target.value)
+}
+
+const choicesList = rpsArr.map((choice, index) => {
+	return <button onClick={handleChoice} value={choice}>{choice}</button>
+});
+
 const Battle = () => {
-	return <h1>Battle Screen</h1>
+	return (
+		<fragment>
+			<h1>Battle Screen</h1>
+			{choicesList}
+		</fragment>
+	)
 }
 
 export default Battle;

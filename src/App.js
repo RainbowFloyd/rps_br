3db994c75e0a1d8fb3a2210377b2329f41683b41
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-08-25 19:16:51 -0700

    Updates opponent state when selecting 1v1 match

--- src/App.js
+++ src/App.js
@@ -8,24 +8,39 @@ import Options from './Components/Options/Options';
 import Battle from './Components/Battle/Battle';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-
+      opponents: {}
     }
+    this.handleOpponentChange = this.handleOpponentChange.bind(this);
   }
+
+
   render() {
     return (
       <div>
         <p>Welcome to Rock, Paper, Scissors Battle Royal!</p>
         <button><Link to='/'>Main Menu</Link></button>
         <Route exact path='/' component={MainMenu} />
-        <Route path='/gamemode' component={ChooseGame} />
+        <Route path='/gamemode' render={()=><ChooseGame 
+          opponents={this.state.opponents}
+          handleOpponentChange={this.handleOpponentChange}
+          />}/>
         <Route path='/options' component={Options} />
         <Route path='/battle' component={Battle} />
       </div>
     )
   }
+
+  handleOpponentChange(newOpponents) {
+    this.setState({
+      opponents: newOpponents
+    })
+    console.log(this.state)
+  }
+
 }
 
 export default App;
--- src/Components/ChooseGame/ChooseGame.js
+++ src/Components/ChooseGame/ChooseGame.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const ChooseGame = () => {
+
+const ChooseGame = (props) => {
+	const createOpponents = (numOfOpponents) => {
+		let newOpponents = {}
+		for (let i = 0; i < numOfOpponents; i++) {
+			let opponentName = `oppenent ${i}`
+			newOpponents[opponentName] = {
+				alive: true
+			}
+		}
+		props.handleOpponentChange(newOpponents);
+	}
 	return(
 		<div>
 			<h1>Choose Game Mode</h1>
-			<button><Link to='/battle'>1v1</Link></button>
+			<button onClick={() => createOpponents(1)}><Link to='/battle'>1v1</Link></button>
 		</div>
 	) 
 
